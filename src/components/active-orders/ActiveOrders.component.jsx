f68d403b5a928459583c798232a833917148eb94
Gleb <38733010+gvalentyuk@users.noreply.github.com>
2020-04-11 21:21:42 +0300

    *

--- src/components/active-orders/ActiveOrders.component.jsx
+++ src/components/active-orders/ActiveOrders.component.jsx
@@ -23,7 +23,7 @@ const ActiveOrders = ({pendingOrders, fetchPendingOrdersAsync}) => {
                             <div key={i} className={
                                 `${order.status === 'Driving' ? 'driving' : order.status === 'Waiting'? 'waiting' : ''} 
                                         active-orders`
-                            } style={i!==0? {'marginTop': '-30px','zIndex': 100 - i}: {'marginTop': '-55px','zIndex': 100 - i}}>
+                            } style={i!==0? {'marginTop': '-30px','zIndex': 100 - i}: {'marginTop': '-65px','zIndex': 100 - i}}>
                                 <OrderInf zIndex={i} {...order}/>
                             </div>
                         )
