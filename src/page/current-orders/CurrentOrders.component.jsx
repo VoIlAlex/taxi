533bfaa36a426de5131f6085ff006bd1299126cb
Glev Valentyuk <gvalentyuk8@gmail.com>
2020-06-29 13:52:43 +0300

    empty orders

--- src/page/current-orders/CurrentOrders.component.jsx
+++ src/page/current-orders/CurrentOrders.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 import Links from '../../components/links/Links.component'
 import TableHead from "../../components/form-input/TableHead.component";
 import TableBody from "../../components/form-input/TableBody.component";
@@ -8,74 +10,77 @@ import './current-orders.style.scss'
 const CurrentOrders = () => {
     //Обработка пустого массива заказов
     const orders = [
-        {
-            taxName: 'OOO "Андрейка"',
-            order: '123456',
-            date: '02.12.20',
-            time: '13:10',
-            status: 'создан',
-            waiting: '12min',
-            number: '+375298220686',
-            filed: 'улица Антонова, 10',
-            where: 'проспект Космонавтов, 283/5',
-            apply: '19:10',
-            onPlace: '15:12',
-            driver: 'Виктор Гаупенко'
-        },
-        {
-            taxName: 'OOO "Андрейка"',
-            order: '123456',
-            date: '02.12.20',
-            time: '13:10',
-            status: 'Ожидание',
-            waiting: '12min',
-            number: '+375298220686',
-            filed: 'улица Антонова, 10',
-            where: 'проспект Космонавтов, 283/5',
-            apply: '19:10',
-            onPlace: '15:12',
-            driver: 'Виктор Гаупенко'
-        },
-        {
-            taxName: 'OOO "Андрейка"',
-            order: '123456',
-            date: '02.12.20',
-            time: '13:10',
-            status: 'Выехал',
-            waiting: '12min',
-            number: '+375298220686',
-            filed: 'улица Антонова, 10',
-            where: 'проспект Космонавтов, 283/5',
-            apply: '19:10',
-            onPlace: '15:12',
-            driver: 'Виктор Гаупенко'
-        },
-        {
-            taxName: 'OOO "Андрейка"',
-            order: '123456',
-            date: '02.12.20',
-            time: '13:10',
-            status: 'создан',
-            waiting: '12min',
-            number: '+375298220686',
-            filed: 'улица Антонова, 10',
-            where: 'проспект Космонавт',
-            apply: '19:10',
-            onPlace: '15:12',
-            driver: 'Виктор Гаупенко'
-        }
+        // {
+        //     taxName: 'OOO "Андрейка"',
+        //     order: '123456',
+        //     date: '02.12.20',
+        //     time: '13:10',
+        //     status: 'создан',
+        //     waiting: '12min',
+        //     number: '+375298220686',
+        //     filed: 'улица Антонова, 10',
+        //     where: 'проспект Космонавтов, 283/5',
+        //     apply: '19:10',
+        //     onPlace: '15:12',
+        //     driver: 'Виктор Гаупенко'
+        // },
+        // {
+        //     taxName: 'OOO "Андрейка"',
+        //     order: '123456',
+        //     date: '02.12.20',
+        //     time: '13:10',
+        //     status: 'Ожидание',
+        //     waiting: '12min',
+        //     number: '+375298220686',
+        //     filed: 'улица Антонова, 10',
+        //     where: 'проспект Космонавтов, 283/5',
+        //     apply: '19:10',
+        //     onPlace: '15:12',
+        //     driver: 'Виктор Гаупенко'
+        // },
+        // {
+        //     taxName: 'OOO "Андрейка"',
+        //     order: '123456',
+        //     date: '02.12.20',
+        //     time: '13:10',
+        //     status: 'Выехал',
+        //     waiting: '12min',
+        //     number: '+375298220686',
+        //     filed: 'улица Антонова, 10',
+        //     where: 'проспект Космонавтов, 283/5',
+        //     apply: '19:10',
+        //     onPlace: '15:12',
+        //     driver: 'Виктор Гаупенко'
+        // },
+        // {
+        //     taxName: 'OOO "Андрейка"',
+        //     order: '123456',
+        //     date: '02.12.20',
+        //     time: '13:10',
+        //     status: 'создан',
+        //     waiting: '12min',
+        //     number: '+375298220686',
+        //     filed: 'улица Антонова, 10',
+        //     where: 'проспект Космонавт',
+        //     apply: '19:10',
+        //     onPlace: '15:12',
+        //     driver: 'Виктор Гаупенко'
+        // }
     ]
     return (
         <>
             <Links/>
-            <div className="table">
-                <table>
-                    <TableHead/>
-                    {
-                        orders.map((order, i) => (<TableBody key={i} {...order}/>))
-                    }
-                </table>
-            </div>
+            {
+                !orders.length ? <h2>У вас пока нет заказов. <Link to={'/'}>Создать?</Link></h2> :
+                    <div className="table">
+                        <table>
+                            <TableHead/>
+                            {
+                                orders.map((order, i) => (<TableBody key={i} {...order}/>))
+                            }
+                        </table>
+                    </div>
+            }
         </>
     )
 }
