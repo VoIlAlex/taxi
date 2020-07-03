9a844b375e366029de5fbfb052a04290310e1395
Glev Valentyuk <gvalentyuk8@gmail.com>
2020-07-03 10:43:12 +0300

    delete logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from "redux-thunk";
-import logger  from 'redux-logger'
 
 import rootReducer from "./root.reducer";
 
-const middleware = [logger, thunk]
+const middleware = [thunk]
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware))
 
